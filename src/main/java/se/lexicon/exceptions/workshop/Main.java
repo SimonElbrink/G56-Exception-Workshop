1e405dbf8307fb2dba4f5265203ec4ec82bffa60
BoKalebsson <bo_kalebsson@hotmail.com>
2025-08-21 13:42:23 +0200

    Simulated adding names and generating a random person in main.

--- src/main/java/se/lexicon/exceptions/workshop/Main.java
+++ src/main/java/se/lexicon/exceptions/workshop/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import se.lexicon.exceptions.workshop.data_access.NameService;
 import se.lexicon.exceptions.workshop.domain.Person;
+import se.lexicon.exceptions.workshop.exception.DuplicateNameException;
 import se.lexicon.exceptions.workshop.fileIO.CSVReader_Writer;
 
 public class Main {
@@ -25,9 +26,41 @@ public class Main {
 
         NameService nameService = new NameService(maleFirstNames, femaleFirstNames, lastNames);
 
+        // Female name:
+        try {
+            nameService.addFemaleFirstName("Linda");
+            System.out.println("Successfully added female name: Linda");
+        } catch (DuplicateNameException e) {
+            System.out.println("Female name already exists: " + e.getMessage());
+        }
 
+        // Male name:
+        try {
+            nameService.addMaleFirstName("Bertil");
+            System.out.println("Successfully added male name: Bertil");
+        } catch (DuplicateNameException e) {
+            System.out.println("Male name already exists: " + e.getMessage());
+        }
+
+        // Last name: Karlsson
+        try {
+            nameService.addLastName("Sigurdsson");
+            System.out.println("Successfully added last name: Sigurdsson");
+        } catch (DuplicateNameException e) {
+            System.out.println("Last name already exists: " + e.getMessage());
+        }
+
+        // Last name: Wallenberg
+        try {
+            nameService.addLastName("Wallenberg");
+            System.out.println("Successfully added last name: Wallenberg");
+        } catch (DuplicateNameException e) {
+            System.out.println("Last name already exists: " + e.getMessage());
+        }
+
+        // Test: generate random person
         Person test = nameService.getNewRandomPerson();
-        System.out.println(test);
+        System.out.println("Generated random person: " + test);
 
     }
 
