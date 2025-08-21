9ca6429738bf480cb862d4d0ed17af8cb9953180
BoKalebsson <bo_kalebsson@hotmail.com>
2025-08-21 10:37:15 +0200

    Implemented a solution for getLastNames().

--- src/main/java/se/lexicon/exceptions/workshop/Main.java
+++ src/main/java/se/lexicon/exceptions/workshop/Main.java
@@ -1,5 +1,6 @@
 package se.lexicon.exceptions.workshop;
 
+import java.io.IOException;
 import java.util.List;
 
 import se.lexicon.exceptions.workshop.data_access.NameService;
@@ -14,7 +15,11 @@ public class Main {
         List<String> maleFirstNames = CSVReader_Writer.getMaleFirstNames();
         List<String> femaleFirstNames = CSVReader_Writer.getFemaleFirstNames();
 
-        List<String> lastNames = CSVReader_Writer.getLastNames();
+        try {
+            List<String> lastNames = CSVReader_Writer.getLastNames();
+        } catch (IOException e) {
+            System.out.println("Could not read last names: " + e.getMessage());;
+        }
 
 
         NameService nameService = new NameService(maleFirstNames, femaleFirstNames, lastNames);
